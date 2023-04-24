efe7449d03f3d770aa5950b6087bfb4e3fa4ad47
Rickelme Dias <rickelmegadias@gmail.com>
2023-04-24 20:58:12 -0300

    Feat: Docs added informations

--- src/main/java/com/octopodius/OctoAPI/utils/SpringDocConfigurations.java
+++ src/main/java/com/octopodius/OctoAPI/utils/SpringDocConfigurations.java
@@ -2,6 +2,9 @@ package com.octopodius.OctoAPI.utils;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +16,15 @@ public class SpringDocConfigurations {
         return new OpenAPI()
                 .components(new Components()
                         .addSecuritySchemes("bearer-key",
-                                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")))
+                .info(new Info()
+                        .title("OctoAPI")
+                        .description("OctoAPI is a Rest API to work with the https://octopodius.com application.")
+                        .contact(new Contact()
+                                .name("Rick Dias - Software Engineer")
+                                .url("https://github.com/RickelmeDias")));
     }
 }
