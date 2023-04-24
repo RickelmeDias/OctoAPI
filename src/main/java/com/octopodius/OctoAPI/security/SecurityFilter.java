c66620f90c19875670707b0605e82238dadc3e8d
Rickelme Dias <rickelmegadias@gmail.com>
2023-04-23 21:39:40 -0300

    Feat: defining the Authentication user when token try authetnicate

--- src/main/java/com/octopodius/OctoAPI/security/SecurityFilter.java
+++ src/main/java/com/octopodius/OctoAPI/security/SecurityFilter.java
@@ -1,11 +1,16 @@
 package com.octopodius.OctoAPI.security;
 
+import com.octopodius.OctoAPI.daos.UserRepository;
+import com.octopodius.OctoAPI.entities.User;
 import com.octopodius.OctoAPI.security.jwt.JwtTokenService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -17,12 +22,18 @@ public class SecurityFilter extends OncePerRequestFilter {
     @Autowired
     private JwtTokenService jwtTokenService;
 
+    @Autowired
+    private UserRepository repository;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String token = getTokenFromHeader(request);
 
         if (token != null) {
-            jwtTokenService.getSubject(token);
+            String username = jwtTokenService.getSubject(token);
+            UserDetails user = repository.findByUsername(username);
+            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
         }
 
         filterChain.doFilter(request, response);
